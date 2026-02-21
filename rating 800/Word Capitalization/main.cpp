c0e483767055f3d7d41c622a270e2ee9c07dda1c
Abdur Rahman <139566806+ARahman-git@users.noreply.github.com>
2026-02-21 11:43:21 +0600

    Add files via upload

---
+++ rating 800/Word Capitalization/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string x;
+    cin>>x;
+    x[0] = toupper(x[0]);
+
+    cout<<x;
+    return 0;
+}
